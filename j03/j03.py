e7bb37362b64feff2b0bce651c8a542471322b0a
youry07 <youry@ukr.net>
2025-06-02 19:44:45 +0300

    j04 added

--- j03/j03.py
+++ j03/j03.py
@@ -2,8 +2,17 @@ import pathlib
 from colorama import Fore, Style
 
 def walk(pref: str, curr_path: pathlib.Path, result: str) -> str:
+    """Обхід та побудова текстової структури директорії
+
+    Аргументи:
+    pref -- відступ рівня структури, 
+    curr_path -- поточний елемент структури, 
+    result -- рядок, де будується текстова структура
+    """
+    # Якщо файл, то просто додаємо рядок 
     if curr_path.is_file():
         result += f"{pref}{Fore.GREEN}{curr_path.name}{Style.RESET_ALL}\n"
+    # Якщо директорія, то додаємо рядок та рекурсивно зпапускаємо її обхід
     elif curr_path.is_dir():
         result += f"{pref}{Fore.BLUE}{curr_path.name}/{Style.RESET_ALL}\n"
         for item in curr_path.iterdir():
@@ -12,17 +21,26 @@ def walk(pref: str, curr_path: pathlib.Path, result: str) -> str:
     return result
 
 
-def dir_info(path) -> str:
+def dir_tree(path) -> str:
+    # Отримаємо об'єкт абсолютного шляху
     root_path = pathlib.Path(path).resolve()
-    result = walk("", root_path, "")
+    # Шлях має існувати і це має бути директорія
+    if root_path.exists() and root_path.is_dir():
+        # Викликаємо рекурсивну функцію обходу та побудови структури
+        result = walk("", root_path, "")
+    else:
+        # Інакше повертаємо повідомлення
+        result = f"{Fore.RED}'{path}' не існує або не директорія{Style.RESET_ALL}"
     return result
 
 import sys
 
 if __name__ == "__main__":
-    path = '..'
+    path = '.'
+    # Якщо є параметр візьмемо його, інакше буде поточна директорія
     if len(sys.argv) > 1:
         path = sys.argv[1]
-    #print(dir_path)
-    result = dir_info(path)
+    # Викликаєио функцію, яка побудує структуру як текст
+    result = dir_tree(path)
+    # Виведемо на консоль
     print(result)    
